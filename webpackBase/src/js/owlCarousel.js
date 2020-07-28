6ea4aef422641a0e9d5301ba05f0d93741ee2ef5
Armen <upravlenciarmen@mail.ru>
2020-07-29 00:05:49 +0400

    header 90%

--- webpackBase/src/js/owlCarousel.js
+++ webpackBase/src/js/owlCarousel.js
@@ -1,11 +1,15 @@
 
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel';
+let back = `
 
+
+`
 $('.owl-carousel').owlCarousel({
     loop:true,
     nav:true,
     items:1,
+    navText:[`<i class="demo-icon icon-left-open">&#xe806;</i>`,`<i class="demo-icon icon-right-open">&#xe800;</i>`]
     
 })
 
