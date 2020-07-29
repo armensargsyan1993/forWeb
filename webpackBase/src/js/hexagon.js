8bd91b2a8bbd76cdfb6dea599a12f5b30e19b922
Armen <upravlenciarmen@mail.ru>
2020-07-30 02:15:17 +0400

    hexagon optional progress

--- webpackBase/src/js/hexagon.js
+++ webpackBase/src/js/hexagon.js
@@ -2,17 +2,25 @@
 function hexagon(obj){
     const block = document.querySelector(`.${obj.block}`)
     const hexagonArr = block.querySelectorAll('.hexagon')
+    let fillInner = null
+    let innerImgTag = ''
     for(let key of hexagonArr){
         let hr = obj.href[0]
         obj.href[0] = obj.url + obj.href[0]
-        key.insertAdjacentHTML('beforeend',`
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 252">
-        <defs>
+        if(!hr.includes('.')){
+            fillInner = hr
+        } else{
+            fillInner = `url(#${hr})`
+            innerImgTag = `<defs>
             <pattern id="${hr}" patternUnits="userSpaceOnUse" width="100%" height="100%">
             <image xlink:href=${obj.href[0]} />
             </pattern>
-        </defs>
-            <path fill="url(#${hr})" d="M75.173,251.959L0,126.152l74.827-126l150-0.193L300,125.766l-74.827,126L75.173,251.959z"/>
+            </defs>` 
+        } 
+        key.insertAdjacentHTML('beforeend',`
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 252">
+            ${innerImgTag}
+            <path fill= ${fillInner} d="M75.173,251.959L0,126.152l74.827-126l150-0.193L300,125.766l-74.827,126L75.173,251.959z"/>
         </svg>
     `)
     obj.href.splice(0,1)
@@ -21,7 +29,35 @@ function hexagon(obj){
 const aboutInnerPhotos = {
     url:'./components/img/about/',
     block: 'about__inner_photos',
-    href: ['1.png','2.png','3.png']
+    href: ['red','2.png','3.png'],
 }
 
 hexagon(aboutInnerPhotos)
+
+
+
+/*
+    for(let key of hexagonArr){
+        let hr = obj.href[0]
+        if(!hr.includes('.')){
+            fillInner = hr
+        } else{
+            innerImgTag = `
+            <defs>
+            <pattern id="${hr}" patternUnits="userSpaceOnUse" width="100%" height="100%">
+            <image xlink:href=${obj.href[0]} />
+            </pattern>
+            </defs>
+            `
+            fillInner = `url(#${hr})`
+        } 
+        obj.href[0] = obj.url + obj.href[0]
+        key.insertAdjacentHTML('beforeend',`
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 252">
+        ${innerImgTag}
+            <path fill= ${fillInner} d="M75.173,251.959L0,126.152l74.827-126l150-0.193L300,125.766l-74.827,126L75.173,251.959z"/>
+        </svg>
+    `)
+    obj.href.splice(0,1)
+    }
+*/
